04b372affee0ca6116a44ddb2f090066d6f61ca3
Ludmila_Shulgina <ludmila_shulgina@list.ru>
2022-04-29 00:01:04 +0300

    styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import PokeCard from './components/PokeCard';
 import PokeGrid from './components/PokeGrid';
 import pockemons from './mock/pockemons.json';
 import { Col, Layout } from 'antd';
-// import * as classnames from 'classnames';
-// import styles from './assets/styles/modules/card.module.css';
+import * as classnames from 'classnames';
+import styles from './assets/styles/modules/card.module.css';
 
 const { Header, Footer, Content } = Layout;
 
@@ -18,7 +18,15 @@ function App() {
           {pockemons.results.map((item, index) => {
             return (
               <Col key={item.name} span={4}>
-                <PokeCard name={index + item.name} url={item.url} />
+                <PokeCard
+                  tag={'yellow'}
+                  name={index + item.name}
+                  url={item.url}
+                  className={classnames('grid', {
+                    [styles.yellow]: index % 2 === 0,
+                    [styles.green]: !(index % 2 === 0),
+                  })}
+                />
               </Col>
             );
           })}
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Card } from 'antd';
 const { Meta } = Card;
+//import dog from '../assets/styles/modules/button.module.css';
+import styles from '../assets/styles/modules/card.module.scss';
 
 import Img from '../assets/images/question.jpg';
 
-const PokeCard = ({ name, url }) => {
+const PokeCard = ({ name, url, tag }) => {
   return (
     <Card
       hoverable
       style={{ width: 240 }}
       cover={<img alt='example' src={Img} />}
+      className={styles[tag]}
     >
       <Meta title={name} description={url} />
     </Card>
